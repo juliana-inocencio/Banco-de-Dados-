5fa78f42bc7bc5605a4b0b88cb126fa397197e0f
juliana-inocencio <jinocencio0911@gmail.com>
2023-04-10 14:36:45 -0300

    Atividade 2 - Pizzaria

--- Atividade 2 -  Pizzaria.sql
+++ Atividade 2 -  Pizzaria.sql
@@ -1,63 +1,68 @@
-INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES ("Maçã", 1000, "2022-03-07", 1.99, 1);
+CREATE DATABASE db_pizzaria_legal;
 
-INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES ("Banana", 1300, "2022-03-08", 5.00, 1);
+CREATE TABLE tb_categorias(
+id BIGINT AUTO_INCREMENT,
+descricao varchar(255) NOT NULL,
 
-INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES ("Batata doce", 2000, "2022-03-09", 10.00, 3);
+PRIMARY KEY (id)
+);
 
-INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES ("Alface", 300, "2022-03-10", 7.00, 2);
+CREATE TABLE tb_produtos(
+id bigint AUTO_INCREMENT,
+sabor varchar(255) NOT NULL,
+pedacos int, 
+vegetariana varchar(255) NOT NULL,
+preco decimal(6, 2),
+categoria_id bigint,
+PRIMARY KEY (id),
+FOREIGN KEY (categoria_id) REFERENCES tb_categorias(id)
+);
 
-INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES ("Cebola", 1020, "2022-03-08", 5.00, 3);
+INSERT INTO tb_categorias(descricao)
+VALUES ("Salgada");
 
-INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES("Ovo Branco", 1000, "2022-03-07", 15.00, 5);
+INSERT INTO tb_categorias(descricao)
+VALUES ("Doce");
 
-INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES("Agrião", 1500, "2022-03-06", 3.00, 2);
+INSERT INTO tb_categorias(descricao)
+VALUES ("Contém lactose");
 
-INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES("Cenoura", 1800, "2022-03-09", 3.50, 3);
+INSERT INTO tb_categorias(descricao)
+VALUES ("Vegana");
 
-INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES ("Pimenta", 1100, "2022-03-15", 10.00, 4);
+INSERT INTO tb_categorias(descricao)
+VALUES ("Contém Glutén");
 
-INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES ("Alecrim", 130, "2022-03-10", 5.00, 4);
+INSERT INTO tb_produtos (sabor, pedacos, vegetariana, preco, categoria_id)
+VALUES ("Mussarela", 8, "Sim", 45, 1);
 
-INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES ("Manga", 200, "2022-03-07", 5.49, 1);
+INSERT INTO tb_produtos (sabor, pedacos, vegetariana, preco, categoria_id)
+VALUES ("Calabresa com cebola", 8, "Não", 45, 1);
 
-INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES ("Laranja", 3000, "2022-03-13", 10.00, 1);
+INSERT INTO tb_produtos (sabor, pedacos, vegetariana, preco, categoria_id)
+VALUES ("Frango com Catupiry", 8, "Não", 50, 3);
 
-INSERT INTO tb_produtos (nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES ("Couve", 100, "2022-03-12", 1.50, 2);
+INSERT INTO tb_produtos (sabor, pedacos, vegetariana, preco, categoria_id)
+VALUES ("Mussarela com rúcula", 8, "Sim", 47, 3);
 
-INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES("Tomate", 1105, "2022-03-15", 3.00, 3);
+INSERT INTO tb_produtos (sabor, pedacos, vegetariana, preco, categoria_id)
+VALUES ("Brocolis com Alho", 8, "Sim", 42, 4);
 
-INSERT INTO tb_produtos(nome, quantidade, dtvalidade, preco, categoria_id)
-VALUES("Rabanete", 1200, "2022-03-15", 13.00, 3);
+INSERT INTO tb_produtos (sabor, pedacos, vegetariana, preco, categoria_id)
+VALUES ("4 Queijos", 8, "Sim", 58, 3);
 
-INSERT INTO tb_produtos(nome, quantidade, preco)
-VALUES("Sacola Cinza", 1118, 0.50);
+INSERT INTO tb_produtos (sabor, pedacos, vegetariana, preco, categoria_id)
+VALUES ("Banana com Queijo e Leite Condensando", 8, "Sim", 62, 2);
 
-INSERT INTO tb_produtos(nome, quantidade, preco)
-VALUES("Sacola Verde", 1118, 0.50);
+INSERT INTO tb_produtos (sabor, pedacos, vegetariana, preco, categoria_id)
+VALUES ("Chocolate com uva verde", 8, "Sim", 63, 2);
 
--- Visualiza todos os dados da tabela tb_produtos
-
-SELECT * FROM tb_produtos ORDER BY nome;
-SELECT * FROM tb_produtos ORDER BY nome DESC;
-SELECT * FROM tb_produtos WHERE nome LIKE "%manga%";
-SELECT * FROM tb_produtos WHERE nome LIKE "%cebola%";
+SELECT * FROM tb_categorias;
+SELECT * FROM tb_produtos;
+SELECT * FROM tb_produtos WHERE sabor LIKE "%m%";
+SELECT * FROM tb_produtos WHERE preco > 45.00;
+SELECT * FROM tb_produtos WHERE preco BETWEEN 50.00 AND 100.00;
 
 SELECT * FROM tb_produtos INNER JOIN tb_categorias 
 ON tb_produtos.categoria_id = tb_categorias.id
 WHERE tb_categorias.id = 2;
-
-
